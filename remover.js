b4c564961e8db3d8b3a500e31c08a36d9f3d92c5
royal <deepakchauhan878@gmail.com>
2020-10-05 21:42:01 +0530

    Updated manifest and script file

--- remover.js
+++ remover.js
@@ -5,7 +5,8 @@ setInterval(function(){
 	console.log("ads detected = " + ads.length)
 	
 	for(i=0; i<ads.length; i++){
-   	 	ads[i].outerHTML = "";
+   	 		ads[i].outerHTML = "";
+			console.log("Ad removed, Ads remaining = " + ads.length);
 		}
 	}
 },3000)
