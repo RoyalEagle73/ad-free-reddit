fec7d6f659ed9537fcac1ae4f48999622340e2cb
royal <deepakchauhan878@gmail.com>
2020-10-08 19:10:40 +0530

    Added code to remove promotions from reddit

--- remover.js
+++ remover.js
@@ -1,12 +1,20 @@
 setInterval(function(){
 	
 	var ads = document.querySelectorAll('[data-before-content="advertisement"]');
-	
-	//console.log("ads detected = " + ads.length)
-	
+	var promotions = document.querySelectorAll('[class*="promoted"]');
+
+	console.log("ads detected = " + ads.length)
+	console.log("promotions detected = " + promotions.length)
+
 	for(i=0; i<ads.length; i++){
    	 		ads[i].outerHTML = "";
-			//console.log("Ad removed, Ads remaining = " + ads.length);
+			console.log("Ad removed, Ads remaining = " + ads.length);
+		}
+	}
+
+	for(i=0; i<promotions.length; i++){
+   	 		promotions[i].outerHTML = "";
+			console.log("promotion removed, promotions remaining = " + promotions.length);
 		}
 	}
 ,3000)
