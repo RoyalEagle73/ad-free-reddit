087fc1fcd2ad0887e5e229ba2ce1e7300a3acc12
royal <deepakchauhan878@gmail.com>
2020-10-09 10:52:34 +0530

    Sorted remover script code for more readability

--- remover.js
+++ remover.js
@@ -2,18 +2,13 @@ setInterval(function(){
 	
 	var ads = document.querySelectorAll('[data-before-content="advertisement"]');
 	var promotions = document.querySelectorAll('[class*="promoted"]');
-
-	//console.log("ads detected = " + ads.length)
-	//console.log("promotions detected = " + promotions.length)
-
+	
+	ads = [...ads,...promotions];
+	
 	for(i=0; i<ads.length; i++){
    	 		ads[i].outerHTML = "";
 			//console.log("Ad removed, Ads remaining = " + ads.length);
-		}
+	}
 
-	for(i=0; i<promotions.length; i++){
-   	 		promotions[i].outerHTML = "";
-			//console.log("promotion removed, promotions remaining = " + promotions.length);
-		}
 	}
 ,3000)
