5e2a188cb2d0119f4990731c1d2774c3cfcfc38b
Deepak Chauhan <deepakchauhan878@gmail.com>
2020-10-11 11:19:32 +0530

    Added support for RPAN ads

--- remover.js
+++ remover.js
@@ -1,14 +1,17 @@
 setInterval(function(){
 	
+	// Finding ads
 	var ads = document.querySelectorAll('[data-before-content="advertisement"]');
-	var promotions = document.querySelectorAll('[class*="promoted"]');
-	
-	ads = [...ads,...promotions];
-	
+	var promotions = document.querySelectorAll('[class*="promoted"]');	
+	var rpan = document.querySelectorAll('[class="_1EG7_Cm18ANvUvJ4rJdRgQ _3Qkp11fjcAw9I9wtLo8frE hrQ5rXDbyl6t3TIXxENXB scrollerItem"]');
+
+	// Concatenating all ads in one
+	ads = [...ads, ...promotions, ...rpan];
+
+	// Deleting all ads
 	for(i=0; i<ads.length; i++){
    	 		ads[i].outerHTML = "";
 			//console.log("Ad removed, Ads remaining = " + ads.length);
 	}
 
-	}
-,3000)
+} ,3000)
